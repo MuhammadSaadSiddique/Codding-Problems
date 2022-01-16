1b9fdc0dfe4050b4b9ddce2f48cc2e9874845e67
Muhammad Saad Siddique <26270646+MuhammaadSaad@users.noreply.github.com>
2022-01-16 11:54:47 +0500

    Time: 1913 ms (5.05%), Space: 15.7 MB (80.65%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.py
+++ 137-single-number-ii/137-single-number-ii.py
@@ -1,13 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = [None,None]
-        a = 0
+        #ans = [None,None]
+        #a = 0
         
         for i in nums:
             if(nums.count(i) == 1):
-                ans[a] = i
-                a+=1
-            if(ans[1] != None):
-                break
+                return i
+                
         
-        return ans[0]
+        return 0
