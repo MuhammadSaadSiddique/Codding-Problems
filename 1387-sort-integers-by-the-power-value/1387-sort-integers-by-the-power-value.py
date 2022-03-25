4255ce5dbc8754de9f4aafec65b7afd9dfd4bedc
Muhammad Saad Siddique <26270646+MuhammaadSaad@users.noreply.github.com>
2022-03-26 00:27:05 +0500

    Time: 2184 ms (5.03%), Space: 14.3 MB (42.16%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,5 +1,6 @@
 class Solution:
     def prsum(self,num,count=0) -> int:
+        
         if(num==4):
             return count+2
         if(num%2==0):
@@ -7,13 +8,12 @@ class Solution:
         else:
             return self.prsum(3*num+1,count+1)
     def getKth(self, lo: int, hi: int, k: int) -> int:
-        maxi={}
-        for i in range(lo,hi+1):
-            c=self.prsum(i,0)
-            
-            maxi.update({i:c})
-        marklist = sorted(maxi.items(), key=lambda x:x[1])
         
-        print(marklist)
-        return marklist[k-1][0]
+        maxi={}
+        for i in range(lo,hi+1):            
+            maxi.update({i:self.prsum(i,0)})
+        maxi = sorted(maxi.items(), key=lambda x:x[1])
+        
+        print(maxi)
+        return maxi[k-1][0]
         
