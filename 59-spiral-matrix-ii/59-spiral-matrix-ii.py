9d204a76614f6f04cc25edb9406eda91c95bfc1d
Muhammad Saad Siddique <26270646+MuhammadSaadSiddique@users.noreply.github.com>
2022-04-14 00:13:20 +0500

    Time: 42 ms (63.42%), Space: 13.8 MB (85.84%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -1,24 +1,33 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        cnt=1
-        res=[ [0 for i in range(n)] for j in range(n) ]  
+        result = [n*[0] for i in range(n)]
         
-        #res.append([i for i in range(1,n+1)])
-        for layer in range(int((n+1)/2)):
-            for ptr in range(layer,n-layer):
-                res[layer][ptr]=cnt
-                cnt+=1
-            for ptr in range(layer+1,n-layer):
-                res[ptr][n-layer-1]=cnt
-                cnt+=1
-            for ptr in range(layer+1,n-layer):
-                res[n-layer-1][n-ptr-1]=cnt
-                cnt+=1
-            for ptr in range(layer+1,n-layer-1):
-                res[n-ptr-1][layer]=cnt
+        nlayers = (n+1) // 2
+        cnt = 1
+        
+        for layer in range(nlayers):
+            #Direction 1: from top left to top right
+            for i in range(n-2*layer):
+                result[layer][i+layer] = cnt
                 cnt+=1
             
-        return res
+            #Direction 2: from top right to bottom right
+            for j in range(n-2*layer - 1):
+                result[layer+1+j][-layer-1] = cnt
+                cnt +=1
+            
+            #Direction 3: from bottom right to bottom left
+            for i in range(n-2*layer-1):
+                result[-layer-1][-layer-2-i] = cnt
+                cnt +=1
+            
+            #Direction 4: from bottom left to top left
+            for j in range(n - 2 *layer - 2):
+                result[-layer-2-j][layer] = cnt
+                cnt +=1
+            
+        
+        return result
             
         
         
