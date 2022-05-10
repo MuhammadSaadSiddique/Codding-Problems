6aab670a1fee0ace41ff52f9fca041813ce1d4b4
Muhammad Saad Siddique <26270646+MuhammadSaadSiddique@users.noreply.github.com>
2022-05-10 13:53:19 +0500

    Time: 6 ms (16.27%), Space: 6.8 MB (20.91%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -4,28 +4,24 @@ class Solution {
 public:
     vector<vector<int>> combinationSum3(int k, int target) {
         vector<int> buffer;
-        vector<int> candidates;
-        for(int j=1;j<=9;j++){
-            candidates.push_back(j);
-            cout<<j;
-        }
-        dfs(candidates, 0, buffer, 0, target,k);
+        
+        dfs( 1, buffer, 0, target,k);
         return result;
     }
    
     
-    void dfs(vector<int> candidates, int i, vector<int>&buffer, int sum, int target,int k){
+    void dfs( int i, vector<int>&buffer, int sum, int target,int k){
         if(target == sum && buffer.size()==k){
             result.push_back(buffer);
             return;
         }
         
-        if(i>=candidates.size() || sum>target)  return;
+        if(i>9 || sum>target)  return;
         
-        buffer.push_back(candidates[i]);
-        dfs(candidates, i+1, buffer, sum+candidates[i], target,k);
+        buffer.push_back(i);
+        dfs( i+1, buffer, sum+i, target,k);
         buffer.pop_back();
         
-        dfs(candidates, i+1, buffer, sum, target,k);
+        dfs( i+1, buffer, sum, target,k);
     }
 };
